29534028cfa234d221961fc545de265a925afb3f
Talha <mdtalha2008@gmail.com>
2022-10-26 12:00:00 +0600

    start

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB8sPIkz6aLHlifb2KxDiwy3vDNeJ-qUIg",
+  authDomain: "educare-bd0b7.firebaseapp.com",
+  projectId: "educare-bd0b7",
+  storageBucket: "educare-bd0b7.appspot.com",
+  messagingSenderId: "761550155899",
+  appId: "1:761550155899:web:05a250007b962132fbb2de"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
