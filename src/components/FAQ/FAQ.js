6668dadac362133e2a54d801ff25160ac451d7a0
Talha <mdtalha2008@gmail.com>
2022-10-27 23:18:14 +0600

    faq done

--- src/components/FAQ/FAQ.js
+++ src/components/FAQ/FAQ.js
@@ -3,7 +3,7 @@ import React from "react";
 const FAQ = () => {
   return (
     <div className="min-h-screen">
-      <h2 className="mt-20">FAQ is Coming Soon</h2>
+      <h2 className="mt-20">FAQ is coming soon</h2>
     </div>
   );
 };
