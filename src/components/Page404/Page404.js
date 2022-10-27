fc22ac11a65f6960eafc828a253a558ccf2c1b91
Talha <mdtalha2008@gmail.com>
2022-10-27 23:21:54 +0600

    page done

--- src/components/Page404/Page404.js
+++ src/components/Page404/Page404.js
@@ -5,7 +5,7 @@ const Page404 = () => {
   return (
     <div>
       <h2 className="text-5xl mt-10 text-red-600">404</h2>
-      <h2 className="text-3xl">Page Not Found</h2>
+      <h2 className="text-3xl">Page Not Found </h2>
       <img
         className="w-4/12 mx-auto"
         src="https://cdn.svgator.com/images/2022/01/funny-404-error-page-design.gif"
